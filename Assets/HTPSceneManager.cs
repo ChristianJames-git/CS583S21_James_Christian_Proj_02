6816daec8da010a913832124154a6f6cbacca195
Christian James <cjames6305@sdsu.edu>
2021-03-16 18:41:47 -0700

    Import Main Scene from Project 1. Change Roll Char to How To Play Added basic scenes framework and Scene Management

---
+++ Assets/HTPSceneManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class HTPSceneManager : MonoBehaviour
+{
+    public List<GameObject> inputPanels;
+    public GameObject mainPanel; //panels
+    private int currentPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputPanels.Add(mainPanel);
+        resetPanels();
+    }
+
+    public void nextPage()
+    {
+        inputPanels[currentPanel++].SetActive(false);
+        if (currentPanel < inputPanels.Count)
+            inputPanels[currentPanel].SetActive(true);
+        else
+            SceneManager.LoadScene("MainScene");
+    }
+
+    private void resetPanels()
+    {
+        for (int i = 1; i < inputPanels.Count; i++)
+        {
+            inputPanels[i].SetActive(false);
+        }
+        currentPanel = 0;
+    }
+}
---
+++ Assets/Scripts/MainSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainSceneManager : MonoBehaviour
+{
+    public GameObject mainPanel;
+
+    public void Start()
+    {
+    }
+
+    public void onPlayClicked()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void onHowToPlayClicked()
+    {
+        SceneManager.LoadScene("HowToPlayScene");
+    }
+
+    public void onQuitClicked()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
