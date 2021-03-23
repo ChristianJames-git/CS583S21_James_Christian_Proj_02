b87da198d1f249946385e1f276494469cf8eb7bc
Christian James <cjames6305@sdsu.edu>
2021-03-22 23:24:34 -0700

    Changed Attack/Mine keybinding to space instead of left-click

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -11,67 +11,53 @@ public class GameSceneManager : MonoBehaviour
     private PlayerStats ps = GameManager.Instance.playerStats;
     float horizontalMove = 0f;
     float verticalMove = 0f;
-    float mouseX;
-    float mouseY;
-    bool mouseClick = false;
-    bool stopClick = false;
+    int facingDir;
     private void Update()
     {
         pos = transform.position;
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
-        mouseClick = Input.GetMouseButtonDown(0);
-        stopClick = Input.GetMouseButtonUp(0);
-        mouseX = Input.mousePosition.x;
-        mouseY = Input.mousePosition.y;
+        if (/*Input.GetMouseButtonDown(0)*/ Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnim.SetBool("Attack?", true);
+            playerAnim.SetInteger("AttackDir", facingDir);
+        }
+        if (/*Input.GetMouseButtonUp(0)*/ Input.GetKeyUp(KeyCode.Space)) 
+        {
+            playerAnim.SetBool("Attack?", false);
+        }
     }
 
     private void FixedUpdate()
     {
         move(horizontalMove, verticalMove);
-        if (mouseClick)
-            attack(mouseX, mouseY);
-        if (stopClick)
-            playerAnim.SetBool("Attack?", false);
-
     }
 
     public void move(float horizontal, float vertical)
     {
         if (horizontal > 0)
+        {
             pos.x += ps.speed * Time.deltaTime;
+            facingDir = 1;
+        }
         if (horizontal < 0)
+        {
             pos.x -= ps.speed * Time.deltaTime;
+            facingDir = 3;
+        }
         if (vertical > 0)
+        {
             pos.y += ps.speed * Time.deltaTime;
+            facingDir = 0;
+        }
         if (vertical < 0)
+        {
             pos.y -= ps.speed * Time.deltaTime;
+            facingDir = 2;
+        }
         transform.position = pos;
         playerAnim.SetInteger("Horizontal", (int)horizontal);
         playerAnim.SetInteger("Vertical", (int)vertical);
-    }
-
-    public void attack(float mouseX, float mouseY)
-    {
-        float horizontal = mouseX - pos.x;
-        float vertical = mouseY - pos.y;
-        float compare = Mathf.Abs(vertical) - Mathf.Abs(horizontal);
-        Debug.Log("compare:" + compare + " vertical:" + Mathf.Abs(vertical) + " horizontal:" + Mathf.Abs(horizontal) + " Vertical:" + mouseY + " Horizontal:" + mouseX);
-        int facingDir;
-        if (compare >= 0)
-        {
-            if (vertical < 0)
-                facingDir = 0;
-            else
-                facingDir = 2;
-        } else {
-            if (horizontal < 0)
-                facingDir = 1;
-            else
-                facingDir = 3;
-        }
-        playerAnim.SetBool("Attack?", true);
-        playerAnim.SetInteger("Direction", facingDir);
     }
 
     public void SavePlayer()
